d0bcb947aa17465122decc5db932042b85d6b5e7
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-01-04 23:11:46 -0300

    Cleared index content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function AkiseizaApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default AkiseizaApp
